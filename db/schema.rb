b4dd479cc9c986ee548331b5a216598f9042932a
alex altieri & julianna green <student@epicodus.com>
2015-09-01 16:11:11 -0700

    Add quiz page, changes response table to have question_id, and make value into a boolean

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901184442) do
+ActiveRecord::Schema.define(version: 20150901230259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,11 +30,11 @@ ActiveRecord::Schema.define(version: 20150901184442) do
 
   create_table "responses", force: :cascade do |t|
     t.integer  "user_id"
-    t.integer  "preference_id"
-    t.integer  "value"
+    t.integer  "question_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "answer"
+    t.boolean  "value"
   end
 
   create_table "users", force: :cascade do |t|
