1ccde19d5d53778e1e9b0c76fb4e4cad31eb5ce7
alex altieri & julianna green <student@epicodus.com>
2015-09-01 12:11:20 -0700

    Add initial integration spec for admin login page

--- spec/integration_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe("the admin path") do
-  it('brings the user to an admin login page') do
-    visit('/')
-    click_button("Admin")
-    expect(page).to have_content("Password")
-  end
-end
