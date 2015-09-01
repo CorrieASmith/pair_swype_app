94b9bdf42e335658d26ab4dc5533de8f5a86649c
alex altieri & julianna green <student@epicodus.com>
2015-09-01 12:37:14 -0700

    Add get routing for admin pages

--- app.rb
+++ app.rb
@@ -6,3 +6,11 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+get('/admin_login') do
+  erb(:admin_login)
+end
+
+get('/admin') do
+  erb(:admin)
+end
--- spec/admin_pages_integration_spec.rb
+++ spec/admin_pages_integration_spec.rb
@@ -1,9 +1,15 @@
 require "spec_helper"
 
-describe('the admin path') do
+describe('the admin path', {:type=> :feature}) do
   it('brings user to the admin login page') do
     visit("/")
-    click_button("Admin")
+    click_link("Admin")
     expect(page).to have_content("Password")
+  end
+
+  it('brings user to the admin page') do
+    visit('/admin_login')
+    click_on('Submit')
+    expect(page).to have_content("Welcome")
   end
 end
