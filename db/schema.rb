25c9549a31d9fcc0274875b37477bda03da073d2
ryan broughan & corrie smith <student@epicodus.com>
2015-09-01 10:38:26 -0700

    add last name column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901170145) do
+ActiveRecord::Schema.define(version: 20150901173442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150901170145) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "last_name"
   end
 
 end
