b12983dd482d6e0512eec0db85ba563bc5b8b386
ryan broughan & corrie smith <student@epicodus.com>
2015-09-02 14:31:37 -0700

    Pairs validate uniqueness of user and partner per day

--- spec/pair_spec.rb
+++ spec/pair_spec.rb
@@ -1,7 +1,22 @@
 require 'spec_helper'
 
 describe(Pair) do
-  it('') do
-    test_user = User.create({name: 'Dave'})
+  describe('#partners') do
+
+    it('') do
+      test_user1 = User.create({name: 'Dave', last_name: 'Daveson', email: 'dave@dave.com', password: 'davedave'})
+      test_user2 = User.create({name: 'Steve', last_name: 'Steveson', email: 'steve@steve.com', password: 'stevesteve'})
+      Pair.create({user_id: test_user1.id, partner_id: test_user2.id, day: "09/01/15"})
+      expect(test_user1.partners).to eq [test_user2]
+    end
+
+    it('') do
+      test_user1 = User.create({name: 'Dave', last_name: 'Daveson', email: 'dave@dave.com', password: 'davedave'})
+      test_user2 = User.create({name: 'Steve', last_name: 'Steveson', email: 'steve@steve.com', password: 'stevesteve'})
+      test_user3 = User.create({name: 'Sean', last_name: 'Seanson', email: 'sean@sean.com', password: 'seansean'})
+      Pair.create({user_id: test_user1.id, partner_id: test_user2.id, day: "09/01/15"})
+      test_pair = Pair.new({user_id: test_user1.id, partner_id: test_user3.id, day: "09/01/15"})
+      expect(test_pair.save).to eq false
+    end
   end
 end
