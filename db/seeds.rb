a4fddfaf0d9ee17f18e79be291d0ed74939df16e
Alex ALtieri <student@epicodus.com>
2015-09-04 09:35:23 -0700

    add questions to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,15 @@ class User < ActiveRecord::Base
   User.create({name: 'Dr. Butt', last_name: 'Swype', cohort_id: 3, email: 'butts@swype.com', password: "tacotaco"})
   User.create({name: 'Dr. UpperLip', last_name: 'Sweat', cohort_id: 3, email: 'moist@drops.com', password: "tacotaco"})
 end
+
+class Question < ActiveRecord::Base
+  Question.create({description: 'I have had experience programming before Epicodus.'})
+  Question.create({description: 'I feel most successful when I am doing the  primary explaining of new concepts.'})
+  Question.create({description: "I feel most successful when I set fire to the rain."})
+  Question.create({description: 'I feel most successful when my partner is doing the primary explaining of new concepts.'})
+  Question.create({description: 'I feel most successful when I may not finish the project but I have a thorough understanding of basic concepts.'})
+  Question.create({description: 'I feel most successful when I finish one project completely.'})
+  Question.create({description: "I feel most successful when I get as many projects done and challenge myself even if I don't fully understand everything by the end of the day."})
+  Question.create({description: "I need to communicate my feelings throughout the day."})
+
+end
